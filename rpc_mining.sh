17f3478658a928678eab1107e0f16c6199571a7c
chienrua1992 <137261530+chienrua1992@users.noreply.github.com>
2024-03-04 10:08:24 +0900

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xeclfweshug2w9scnvxt9 -r dero.rabidmining.com:10300 -r1 sg1.fastpool.xyz:10300 -p rpc;
+    ./astrominer -w dero1qyj5my7lgqkgdulrr2we2rr2p8n77uxncws0vc83mrylthrt2gfrwqgq9e3p -r community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -r3 sg1.fastpool.xyz:10300 -m 7 -p rpc;
     sleep 5;
 done
